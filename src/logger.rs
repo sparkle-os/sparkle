9bf5418844a7492b5b2e5ffd671a50e861a4ed96
Erin <erin@hashbang.sh>
2018-03-21 21:06:34 -0500

    log to serial and vga; make each feature-flagged

--- src/logger.rs
+++ src/logger.rs
@@ -3,33 +3,42 @@
 use log;
 use log::{Log, LogLevelFilter, LogMetadata, LogRecord};
 use log::SetLoggerError;
+use arch::x86_64::device::serial::COM1;
 
-/// Initializes the VGA console logger at kernel boot.
+/// Initializes the logger at kernel boot.
 pub fn init() -> Result<(), SetLoggerError> {
     unsafe {
         log::set_logger_raw(|max_log_level| {
-            static LOGGER: ConsoleLogger = ConsoleLogger;
+            static LOGGER: KernelLogger = KernelLogger;
             max_log_level.set(LOGGER.filter());
             &LOGGER
         })
     }
 }
 
-/// A `log` logger, which dumps to the VGA console.
-struct ConsoleLogger;
-impl ConsoleLogger {
+/// The kernel-level logger.
+struct KernelLogger;
+impl KernelLogger {
     fn filter(&self) -> LogLevelFilter {
         LogLevelFilter::Debug
     }
 }
-impl Log for ConsoleLogger {
+impl Log for KernelLogger {
     fn enabled(&self, metadata: &LogMetadata) -> bool {
         metadata.level() <= ::misc::LOG_LEVEL
     }
 
     fn log(&self, record: &LogRecord) {
         if self.enabled(record.metadata()) {
-            println!("{}: {}", record.level(), record.args());
+            #[cfg(feature = "logging-serial")]
+            {
+                use core::fmt::Write;
+                write!(COM1.write(), "[{}]: {}\n", record.level(), record.args());
+            }
+            #[cfg(feature = "logging-console")]
+            {
+                println!("{}: {}", record.level(), record.args());
+            }
         }
     }
 }
