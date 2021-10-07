772492c2a162315cad201bd3186861a21f6d65fa
Erin Moon <erin@hecke.rs>
2021-10-07 10:07:20 -0500

    fb test

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 //! The Sparkle microkernel.
-#![feature(lang_items)]
+#![feature(lang_items, asm)]
 #![no_std]
 #![no_main]
 
 use core::panic::PanicInfo;
+use bootloader::boot_info::BootInfo;
 
 /// Kernel entry point
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _start(boot_info: &'static mut BootInfo) -> ! {
+    if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
+        for byte in framebuffer.buffer_mut() {
+            *byte = 0x90;
+        }
+    }
+
     // spin
     loop {
-        x86_64::instructions::hlt();
     }
 }
 
